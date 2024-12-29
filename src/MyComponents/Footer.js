5fb0f33bb487e144232d55a98b9a53b563dcf745
Yewantraj Karki <54407001+yewant@users.noreply.github.com>
2024-12-29 15:24:08 -0500

    footer added

--- src/MyComponents/Footer.js
+++ src/MyComponents/Footer.js
@@ -1,10 +1,18 @@
 import React from 'react'
 
 const Footer = () => {
+  let footerStyle = {
+    position: "absolute",
+    top: "100vh",
+    width: "100%",
+    padding: "20px"
+  }
   return (
-    <div>
-      footer works!
-    </div>
+    <footer className='bg-dark text-light' style={footerStyle}>
+      <p className='text-center'>
+        Copyright &copy; MyTodoList.com
+      </p>
+    </footer>
   )
 }
 
